df4317fc09e4fdc72e79f498deda4430cea35543
Kate505 <ksdelgado31@gmail.com>
2023-01-12 22:58:47 -0600

    new file:   models/tarea.js 	new file:   models/tareas.js 	modified:   package-lock.json 	modified:   package.json

---
+++ models/tarea.js
@@ -0,0 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+export class Tarea {
+    id = '';
+    desc = '';
+    completadoEn = null;
+
+    constructor( desc ){
+        this.id = uuidv4();
+        this.desc = desc;
+        this.completadoEn = null;
+    }
+}
---
+++ models/tareas.js
@@ -0,0 +1,14 @@
+/*
+    _listado: 
+        {'uuid-254-654654-6546: {desc:asd, completadoEn:9846} },
+
+*/
+
+export class Tareas {
+    _listado = {};
+
+    constructor(){
+        this._listado = {};
+    }
+
+}
